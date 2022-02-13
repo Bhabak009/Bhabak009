b66641907e0f1f7d9d47c46d0d8abdb442aace7c
suman-majhi <sumanmajhi3006@gmail.com>
2022-02-13 20:02:28 +0530

    basic setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,28 @@
 module.exports = {
-  root: true,
+  root: false,
   env: {
     browser: true,
     node: true
   },
   parserOptions: {
-    parser: '@babel/eslint-parser',
-    requireConfigFile: false
+    parser: 'babel-eslint'
   },
   extends: [
-    '@nuxtjs',
-    'plugin:nuxt/recommended'
+    '@nuxtjs', 'plugin:nuxt/recommended'
   ],
   plugins: [
   ],
   // add your custom rules here
-  rules: {}
+  rules: {
+    'comma-dangle': [2, 'never'],
+    'no-inline-styles': 0,
+    'prettier/prettier': 0,
+    semi: [2, 'never'],
+    'no-debugger': [0, 2],
+    'array-element-newline': ['error',
+      {
+        multiline: true,
+        minItems: 3
+      }]
+  }
 }
---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div>My blog navigation bar here</div>
+    <Nuxt />
+  </div>
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  font-family: sans-serif;
+}
+</style>
