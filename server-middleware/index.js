7e5f9cf1567d9ceb651d1478b5398baa6f7f621e
Souradeep Dutta <souradeepdutta2001@gmail.com>
2022-02-23 20:27:16 +0530

    changed index.js

--- server-middleware/index.js
+++ server-middleware/index.js
@@ -2,6 +2,6 @@ const axios = require('axios')
 const app = require('express')()
 
 app.get("/", (req, resp) => {
-    resp.send("<h1>this is index file</h1>")
+    resp.send("<h1>this is test index file</h1>")
 })
 module.exports = app
