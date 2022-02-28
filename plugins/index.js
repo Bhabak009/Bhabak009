491f4922cc2f145898a011b9fe58d73915fca056
souvik <souvik.dhang@gmail.com>
2022-02-28 22:03:10 +0530

    added google auth

---
+++ plugins/index.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyADCZSDj0wHLCkJU5W6HN2iyo7y978SRec",
+  authDomain: "smartenergyconservation.firebaseapp.com",
+  databaseURL: "https://smartenergyconservation-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "smartenergyconservation",
+  storageBucket: "smartenergyconservation.appspot.com",
+  messagingSenderId: "366935591012",
+  appId: "1:366935591012:web:a62fd1b589eb476111b771",
+  measurementId: "G-Y7MVLQNQF7"
+};
+const app = initializeApp(firebaseConfig);
