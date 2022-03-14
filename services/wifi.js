091cdad159a41e5f24d254b238f5e1f0ba283ffd
souvikdhang <51173794+souvikdhang@users.noreply.github.com>
2022-03-14 21:57:16 +0530

    Update wifi.js

--- services/wifi.js
+++ services/wifi.js
@@ -1,18 +1,27 @@
-import axios from 'axios'
-
+const https = require('https');
+const axios = require('axios')
+const instance = axios.create({
+  httpsAgent: new https.Agent({  
+    rejectUnauthorized: false
+  })
+});
+const agent = new https.Agent({  
+  rejectUnauthorized: false
+});
 export const scanWifi = (payload) => {
   const options = {
     method: 'GET',
-    url: 'http://192.168.4.1/scan'
+    url: 'http://192.168.4.1/scan',
+    httpsAgent: agent
   }
   return axios(options)
 }
-
 export const setUserId = (payload) => {
   const options = {
     method: 'POST',
     url: 'http://192.168.4.1/setuid',
-    header: payload
+    header: payload,
+    httpsAgent: agent,
   }
   return axios(options)
 }
@@ -21,7 +30,8 @@ export const setSsid = (payload) => {
   const options = {
     method: 'POST',
     url: 'http://192.168.4.1/setuid',
-    header: payload
+    header: payload,
+    httpsAgent: agent
   }
   return axios(options)
 }
