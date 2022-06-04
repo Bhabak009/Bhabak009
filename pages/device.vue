bcdb95b3cd509706d81f27e0d814f8285ff3c58d
suman-majhi <sumanmajhi3006@gmail.com>
2022-06-04 22:08:23 +0530

    to int for power

--- pages/device.vue
+++ pages/device.vue
@@ -68,7 +68,7 @@ export default {
         // finalData.push(JSON.parse(element))
         if (!element) return
         const time = new Date(element.split(':')[0]*1000)
-        const power = element.split(':')[1]
+        const power = element.split(':')[1] - 0
         lastPower += power
         finalData[time] = lastPower
       });
